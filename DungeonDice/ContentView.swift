ab2fee87fec990dfb1ba0b96a311c51e039df439
myMacosGit <reg.isaacs@gmail.com>
2022-09-29 19:06:15 +0200

    version 2.5

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -23,38 +23,122 @@ struct ContentView: View {
         }
     } // Dice
     
+    @State private var columns = [
+        GridItem(.adaptive(minimum: 100))  ] // not needed
+    
     @State private var resultMessage = ""
     // @State private var players = ["player1","player2","player3","player4","player5"]
     
+    @State private var buttonsLeftOver = 0 // buttons in full row
+    
+    let horizontalPadding: CGFloat = 16
+    let spacing: CGFloat = 0  // between buttons
+    let buttonWidth: CGFloat = 102 // 130
     
     var body: some View {
-        VStack {
-            Text("Dungeon Dice")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundColor(.red)
-            
-            Spacer()
-            
-            Text(resultMessage)
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .multilineTextAlignment(.center)
-                .frame(height: 150)
-            
-            Spacer()
-            
-            ForEach(Dice.allCases, id: \.self) { dice in
-                Button("\(dice.rawValue)-sided") {
-                    resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+        GeometryReader { geo in
+            VStack {
+                Text("Dungeon Dice")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.red)
+                
+                Spacer()
+                
+                Text(resultMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                    .frame(height: 150)
+                
+                Spacer()
+                
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: buttonWidth),spacing: spacing   )  ]) {
+                    ///LazyVGrid(columns: columns) {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
                 }
-            }
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
-        }
-        .padding()
+                
+                HStack {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
+                        
+                        .buttonStyle(.borderedProminent)
+                        .tint(.red)
+                    }
+                } // HStack
+                
+            } // VStack
+            .padding()
+            .onChange(of: geo.size.width, perform: { newValue in
+                
+                
+//                var screenWidth = geo.size.width - horizontalPadding * 2 // padding on both sides
+//                if Dice.allCases.count > 1 {
+//                    screenWidth += spacing
+//                }
+//
+//                // caluulate numOfButtonsPerRow as a Int
+//                let numOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+//                buttonsLeftOver = Dice.allCases.count % numOfButtonsPerRow
+//
+//                print("numOfButtonsPerRow = \(numOfButtonsPerRow)")
+//                print("buttonsLeftOver = \(buttonsLeftOver)")
+                
+                arrangeGridItems(geo: geo)
+                
+                
+            }) // onChange
+            .onAppear {
+//                var screenWidth = geo.size.width - horizontalPadding * 2 // padding on both sides
+//                if Dice.allCases.count > 1 {
+//                    screenWidth += spacing
+//                }
+//
+//                // caluulate numOfButtonsPerRow as a Int
+//                let numOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+//                buttonsLeftOver = Dice.allCases.count % numOfButtonsPerRow
+//
+//                print("numOfButtonsPerRow = \(numOfButtonsPerRow)")
+//                print("buttonsLeftOver = \(buttonsLeftOver)")
+                
+                arrangeGridItems(geo: geo)
+                
+            } // onAppear
+        } // geo
+        
     } // body
-} // ContentView
+    
+    
+    
+    func arrangeGridItems(geo: GeometryProxy) {
+        var screenWidth =  geo.size.width - horizontalPadding * 2 // padding on both sides
+                if Dice.allCases.count > 1 {
+                    screenWidth += spacing
+                }
+                
+                // caluulate numOfButtonsPerRow as a Int
+                let numOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+                buttonsLeftOver = Dice.allCases.count % numOfButtonsPerRow
+                
+                print("numOfButtonsPerRow = \(numOfButtonsPerRow)")
+                print("buttonsLeftOver = \(buttonsLeftOver)")
+
+            }
+
+    
+    
+    
+} // struct ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
